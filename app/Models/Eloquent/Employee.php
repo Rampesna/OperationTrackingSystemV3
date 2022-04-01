5218a349c647e82ab47c9dd9f2cbd424ca1df793
Talha Can <rampesna@gmail.com>
2022-04-01 17:08:03 +0300

    v3.2.207.0

--- app/Models/Eloquent/Employee.php
+++ app/Models/Eloquent/Employee.php
@@ -3,11 +3,12 @@
 namespace App\Models\Eloquent;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class Employee extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 }
