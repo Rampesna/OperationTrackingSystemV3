f94aa9337524921f538b64775383910edce5984d
Talha Can <rampesna@gmail.com>
2022-07-13 14:06:07 +0300

    v3.55.36.128

--- app/Services/Eloquent/OvertimeTypeService.php
+++ app/Services/Eloquent/OvertimeTypeService.php
@@ -4,25 +4,76 @@ namespace App\Services\Eloquent;
 
 use App\Interfaces\Eloquent\IOvertimeTypeService;
 use App\Models\Eloquent\OvertimeType;
+use App\Services\ServiceResponse;
 
 class OvertimeTypeService implements IOvertimeTypeService
 {
-    public function getAll()
+    /**
+     * @return ServiceResponse
+     */
+    public function getAll(): ServiceResponse
     {
-        return OvertimeType::all();
+        return new ServiceResponse(
+            true,
+            'All overtime types',
+            200,
+            OvertimeType::all()
+        );
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function getById(
         int $id
-    )
+    ): ServiceResponse
     {
-        return OvertimeType::find($id);
+        $overtimeType = OvertimeType::find($id);
+
+        if ($overtimeType) {
+            return new ServiceResponse(
+                true,
+                'Overtime type',
+                200,
+                $overtimeType
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Overtime type not found',
+                404,
+                null
+            );
+        }
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function delete(
         int $id
-    )
+    ): ServiceResponse
     {
-        return $this->getById($id)->delete();
+        $overtimeType = $this->getById($id);
+
+        if ($overtimeType->isSuccess()) {
+            return new ServiceResponse(
+                true,
+                'Overtime type deleted',
+                200,
+                $overtimeType->getData()->delete()
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Overtime type not found',
+                404,
+                null
+            );
+        }
     }
 }
