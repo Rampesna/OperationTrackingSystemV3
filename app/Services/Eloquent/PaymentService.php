8e1e8297499a23e42f06f1e06e3e7cf60dba3f34
Talha Can <rampesna@gmail.com>
2022-07-13 15:00:16 +0300

    v3.55.36.130

--- app/Services/Eloquent/PaymentService.php
+++ app/Services/Eloquent/PaymentService.php
@@ -4,32 +4,91 @@ namespace App\Services\Eloquent;
 
 use App\Interfaces\Eloquent\IPaymentService;
 use App\Models\Eloquent\Payment;
+use App\Services\ServiceResponse;
 
 class PaymentService implements IPaymentService
 {
-    public function getAll()
+    /**
+     * @return ServiceResponse
+     */
+    public function getAll(): ServiceResponse
     {
-        return Payment::all();
+        return new ServiceResponse(
+            true,
+            'All payments',
+            200,
+            Payment::all()
+        );
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function getById(
         int $id
-    )
+    ): ServiceResponse
     {
-        return Payment::with([
+        $payment = Payment::with([
             'type',
             'status',
             'employee',
         ])->find($id);
+        if ($payment) {
+            return new ServiceResponse(
+                true,
+                'Payment',
+                200,
+                $payment
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Payment not found',
+                404,
+                null
+            );
+        }
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function delete(
         int $id
-    )
+    ): ServiceResponse
     {
-        return $this->getById($id)->delete();
+        $payment = $this->getById($id);
+        if ($payment->isSuccess()) {
+            return new ServiceResponse(
+                true,
+                'Payment deleted',
+                200,
+                $payment->getData()->delete()
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Payment not found',
+                404,
+                null
+            );
+        }
     }
 
+    /**
+     * @param int $employeeId
+     * @param int $typeId
+     * @param int $statusId
+     * @param string $date
+     * @param int $amount
+     * @param string $description
+     *
+     * @return ServiceResponse
+     */
     public function create(
         int    $employeeId,
         int    $typeId,
@@ -37,7 +96,7 @@ class PaymentService implements IPaymentService
         string $date,
         int    $amount,
         string $description
-    )
+    ): ServiceResponse
     {
         $payment = new Payment;
         $payment->employee_id = $employeeId;
@@ -48,9 +107,25 @@ class PaymentService implements IPaymentService
         $payment->description = $description;
         $payment->save();
 
-        return $payment;
+        return new ServiceResponse(
+            true,
+            'Payment created',
+            201,
+            $payment
+        );
     }
 
+    /**
+     * @param int $id
+     * @param int $employeeId
+     * @param int $typeId
+     * @param int $statusId
+     * @param string $date
+     * @param int $amount
+     * @param string $description
+     *
+     * @return ServiceResponse
+     */
     public function update(
         int    $id,
         int    $employeeId,
@@ -59,34 +134,55 @@ class PaymentService implements IPaymentService
         string $date,
         int    $amount,
         string $description
-    )
+    ): ServiceResponse
     {
-        $payment = Payment::find($id);
-        $payment->employee_id = $employeeId;
-        $payment->type_id = $typeId;
-        $payment->status_id = $statusId;
-        $payment->date = $date;
-        $payment->amount = $amount;
-        $payment->description = $description;
-        $payment->save();
+        $payment = $this->getById($id);
+        if ($payment->isSuccess()) {
+            $payment->getData()->employee_id = $employeeId;
+            $payment->getData()->type_id = $typeId;
+            $payment->getData()->status_id = $statusId;
+            $payment->getData()->date = $date;
+            $payment->getData()->amount = $amount;
+            $payment->getData()->description = $description;
+            $payment->getData()->save();
 
-        return $payment;
+            return new ServiceResponse(
+                true,
+                'Payment updated',
+                200,
+                $payment->getData()
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Payment not found',
+                404,
+                null
+            );
+        }
     }
 
     /**
      * @param int $employeeId
      * @param string $startDate
      * @param string $endDate
+     *
+     * @return ServiceResponse
      */
     public function getDateBetween(
         int    $employeeId,
         string $startDate,
         string $endDate
-    )
+    ): ServiceResponse
     {
-        return Payment::with([
-            'status',
-            'type'
-        ])->where('employee_id', $employeeId)->whereBetween('date', [$startDate, $endDate])->get();
+        return new ServiceResponse(
+            true,
+            'Payments between dates',
+            200,
+            Payment::with([
+                'status',
+                'type'
+            ])->where('employee_id', $employeeId)->whereBetween('date', [$startDate, $endDate])->get()
+        );
     }
 }
