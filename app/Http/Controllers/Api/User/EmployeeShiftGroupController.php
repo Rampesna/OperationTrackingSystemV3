462761010fb4fba08c43ac47338a7c76081ba1b2
Talha Can <rampesna@gmail.com>
2022-07-14 17:07:37 +0300

    v3.55.36.234

--- app/Http/Controllers/Api/User/EmployeeShiftGroupController.php
+++ app/Http/Controllers/Api/User/EmployeeShiftGroupController.php
@@ -15,33 +15,107 @@ class EmployeeShiftGroupController extends Controller
 {
     use Response;
 
+    /**
+     * @var $employeeService
+     */
     private $employeeService;
+
+    /**
+     * @var $shiftGroupService
+     */
     private $shiftGroupService;
 
-    public function __construct(IEmployeeService $employeeService, IShiftGroupService $shiftGroupService)
+    /**
+     * @param IEmployeeService $employeeService
+     * @param IShiftGroupService $shiftGroupService
+     */
+    public function __construct(
+        IEmployeeService   $employeeService,
+        IShiftGroupService $shiftGroupService
+    )
     {
         $this->employeeService = $employeeService;
         $this->shiftGroupService = $shiftGroupService;
     }
 
+    /**
+     * @param GetEmployeeShiftGroupsRequest $request
+     */
     public function getEmployeeShiftGroups(GetEmployeeShiftGroupsRequest $request)
     {
-        return $this->success('Employee shiftGroups', $this->employeeService->getEmployeeShiftGroups($request->employeeId));
+        $getEmployeeShiftGroupsResponse = $this->employeeService->getEmployeeShiftGroups($request->employeeId);
+        if ($getEmployeeShiftGroupsResponse->isSuccess()) {
+            return $this->success(
+                $getEmployeeShiftGroupsResponse->getMessage(),
+                $getEmployeeShiftGroupsResponse->getData(),
+                $getEmployeeShiftGroupsResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getEmployeeShiftGroupsResponse->getMessage(),
+                $getEmployeeShiftGroupsResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param SetEmployeeShiftGroupsRequest $request
+     */
     public function setEmployeeShiftGroups(SetEmployeeShiftGroupsRequest $request)
     {
-        return $this->success('Employee shiftGroups updated', $this->employeeService->setEmployeeShiftGroups($request->employeeId, $request->shiftGroupIds));
+        $setEmployeeShiftGroupsResponse = $this->employeeService->setEmployeeShiftGroups($request->employeeId, $request->shiftGroupIds);
+        if ($setEmployeeShiftGroupsResponse->isSuccess()) {
+            return $this->success(
+                $setEmployeeShiftGroupsResponse->getMessage(),
+                $setEmployeeShiftGroupsResponse->getData(),
+                $setEmployeeShiftGroupsResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $setEmployeeShiftGroupsResponse->getMessage(),
+                $setEmployeeShiftGroupsResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetShiftGroupEmployeesRequest $request
+     */
     public function getShiftGroupEmployees(GetShiftGroupEmployeesRequest $request)
     {
-        return $this->success('ShiftGroup employees', $this->shiftGroupService->getShiftGroupEmployees($request->shiftGroupId));
+        $getShiftGroupEmployeesResponse = $this->shiftGroupService->getShiftGroupEmployees($request->shiftGroupId);
+        if ($getShiftGroupEmployeesResponse->isSuccess()) {
+            return $this->success(
+                $getShiftGroupEmployeesResponse->getMessage(),
+                $getShiftGroupEmployeesResponse->getData(),
+                $getShiftGroupEmployeesResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getShiftGroupEmployeesResponse->getMessage(),
+                $getShiftGroupEmployeesResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param SetShiftGroupEmployeesRequest $request
+     */
     public function setShiftGroupEmployees(SetShiftGroupEmployeesRequest $request)
     {
-        return $this->success('ShiftGroup employees updated', $this->shiftGroupService->setShiftGroupEmployees($request->shiftGroupId, $request->employeeIds));
+        $setShiftGroupEmployeesResponse = $this->shiftGroupService->setShiftGroupEmployees($request->shiftGroupId, $request->employeeIds);
+        if ($setShiftGroupEmployeesResponse->isSuccess()) {
+            return $this->success(
+                $setShiftGroupEmployeesResponse->getMessage(),
+                $setShiftGroupEmployeesResponse->getData(),
+                $setShiftGroupEmployeesResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $setShiftGroupEmployeesResponse->getMessage(),
+                $setShiftGroupEmployeesResponse->getStatusCode()
+            );
+        }
     }
 }
 
