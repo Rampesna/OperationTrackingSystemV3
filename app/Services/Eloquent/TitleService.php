f4a355f5cf896e1c99b8653d555cc9e52876bfe9
Talha Can <rampesna@gmail.com>
2022-07-14 11:19:30 +0300

    v3.55.36.146

--- app/Services/Eloquent/TitleService.php
+++ app/Services/Eloquent/TitleService.php
@@ -4,58 +4,138 @@ namespace App\Services\Eloquent;
 
 use App\Interfaces\Eloquent\ITitleService;
 use App\Models\Eloquent\Title;
+use App\Services\ServiceResponse;
 
 class TitleService implements ITitleService
 {
-    public function getAll()
+    /**
+     * @return ServiceResponse
+     */
+    public function getAll(): ServiceResponse
     {
-        return Title::all();
+        return new ServiceResponse(
+            true,
+            'All titles',
+            200,
+            Title::all()
+        );
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function getById(
         int $id
-    )
+    ): ServiceResponse
     {
-        return Title::find($id);
+        $title = Title::find($id);
+        if ($title) {
+            return new ServiceResponse(
+                true,
+                'Title',
+                200,
+                $title
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Title not found',
+                404,
+                null
+            );
+        }
     }
 
+    /**
+     * @param int $departmentId
+     *
+     * @return ServiceResponse
+     */
     public function getByDepartmentId(
         int $departmentId
-    )
+    ): ServiceResponse
     {
-        return Title::where('department_id', $departmentId)->get();
+        return new ServiceResponse(
+            true,
+            'Titles',
+            200,
+            Title::where('department_id', $departmentId)->get()
+        );
     }
 
+    /**
+     * @param int $departmentId
+     * @param string $name
+     *
+     * @return ServiceResponse
+     */
     public function create(
         int    $departmentId,
         string $name
-    )
+    ): ServiceResponse
     {
         $title = new Title;
         $title->department_id = $departmentId;
         $title->name = $name;
         $title->save();
 
-        return $title;
+        return new ServiceResponse(
+            true,
+            'Title created',
+            201,
+            $title
+        );
     }
 
+    /**
+     * @param int $id
+     * @param string $name
+     *
+     * @return ServiceResponse
+     */
     public function update(
         int    $id,
         string $name
-    )
+    ): ServiceResponse
     {
         $title = $this->getById($id);
-        $title->name = $name;
-        $title->save();
+        if ($title->isSuccess()) {
+            $title->getData()->name = $name;
+            $title->getData()->save();
 
-        return $title;
+            return new ServiceResponse(
+                true,
+                'Title updated',
+                200,
+                $title->getData()
+            );
+        } else {
+            return $title;
+        }
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function delete(
         int $id
-    )
+    ): ServiceResponse
     {
-        return $this->getById($id)->delete();
+        $title = $this->getById($id);
+        if ($title->isSuccess()) {
+            return new ServiceResponse(
+                true,
+                'Title deleted',
+                200,
+                $title->getData()->delete()
+            );
+        } else {
+            return $title;
+        }
     }
 
 }
