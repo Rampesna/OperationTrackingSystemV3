1101be1db4ed636e58feb996f1831a6b2ffa61c2
Talha Can <rampesna@gmail.com>
2022-07-14 16:45:55 +0300

    v3.55.36.221

--- app/Http/Controllers/Api/User/OperationApi/TvScreenController.php
+++ app/Http/Controllers/Api/User/OperationApi/TvScreenController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\User\OperationApi;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\User\OperationApi\TvScreenController\GetJobListRequest;
 use App\Interfaces\Eloquent\IJobDepartmentService;
 use App\Interfaces\OperationApi\ITvScreenService;
 use App\Http\Requests\Api\User\OperationApi\TvScreenController\GetStaffStatusListRequest;
@@ -13,28 +14,74 @@ class TvScreenController extends Controller
 {
     use Response;
 
+    /**
+     * @var $tvScreenService
+     */
     private $tvScreenService;
 
+    /**
+     * @var $jobDepartmentService
+     */
     private $jobDepartmentService;
 
-    public function __construct(ITvScreenService $tvScreenService, IJobDepartmentService $jobDepartmentService)
+    /**
+     * @param ITvScreenService $tvScreenService
+     * @param IJobDepartmentService $jobDepartmentService
+     */
+    public function __construct(
+        ITvScreenService      $tvScreenService,
+        IJobDepartmentService $jobDepartmentService
+    )
     {
         $this->tvScreenService = $tvScreenService;
         $this->jobDepartmentService = $jobDepartmentService;
     }
 
-    public function getJobList()
+    /**
+     * @param GetJobListRequest $request
+     */
+    public function getJobList(GetJobListRequest $request)
     {
-        return $this->success('Job list', $this->tvScreenService->GetJobList());
+        $getJobListResponse = $this->tvScreenService->GetJobList();
+        if ($getJobListResponse->isSuccess()) {
+            return $this->success(
+                $getJobListResponse->getMessage(),
+                $getJobListResponse->getData(),
+                $getJobListResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getJobListResponse->getMessage(),
+                $getJobListResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetStaffStatusListRequest $request
+     */
     public function getStaffStatusList(GetStaffStatusListRequest $request)
     {
-        return $this->success('Staff status list', $this->tvScreenService->GetStaffStatusList(
+        $getStaffStatusListResponse = $this->tvScreenService->GetStaffStatusList(
             $request->companyIds
-        ));
+        );
+        if ($getStaffStatusListResponse->isSuccess()) {
+            return $this->success(
+                $getStaffStatusListResponse->getMessage(),
+                $getStaffStatusListResponse->getData(),
+                $getStaffStatusListResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getStaffStatusListResponse->getMessage(),
+                $getStaffStatusListResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetStaffStatusUserListRequest $request
+     */
     public function getStaffStatusUserList(GetStaffStatusUserListRequest $request)
     {
 //        $jobDepartments = $this->jobDepartmentService->getByTypeIds($request->employeeGuids);
@@ -46,8 +93,20 @@ class TvScreenController extends Controller
 //            }
 //        }
 
-        return $this->success('Staff status user list', $this->tvScreenService->GetStaffStatusUserList(
+        $getStaffStatusUserListResponse = $this->tvScreenService->GetStaffStatusUserList(
             $request->employeeGuids
-        ));
+        );
+        if ($getStaffStatusUserListResponse->isSuccess()) {
+            return $this->success(
+                $getStaffStatusUserListResponse->getMessage(),
+                $getStaffStatusUserListResponse->getData(),
+                $getStaffStatusUserListResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getStaffStatusUserListResponse->getMessage(),
+                $getStaffStatusUserListResponse->getStatusCode()
+            );
+        }
     }
 }
