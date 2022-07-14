f55243ea7f4a9ef45fe0e1b9cdecdead46307468
Talha Can <rampesna@gmail.com>
2022-07-14 17:26:42 +0300

    v3.55.36.245

--- app/Http/Controllers/Api/User/TitleController.php
+++ app/Http/Controllers/Api/User/TitleController.php
@@ -16,52 +16,148 @@ class TitleController extends Controller
 {
     use Response;
 
+    /**
+     * @var $titleService
+     */
     private $titleService;
 
+    /**
+     * @param ITitleService $titleService
+     */
     public function __construct(ITitleService $titleService)
     {
         $this->titleService = $titleService;
     }
 
+    /**
+     * @param GetAllRequest $request
+     */
     public function getAll(GetAllRequest $request)
     {
-        return $this->success('Titles', $this->titleService->getAll());
+        $getAllResponse = $this->titleService->getAll();
+        if ($getAllResponse->isSuccess()) {
+            return $this->success(
+                $getAllResponse->getMessage(),
+                $getAllResponse->getData(),
+                $getAllResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getAllResponse->getMessage(),
+                $getAllResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetByIdRequest $request
+     */
     public function getById(GetByIdRequest $request)
     {
-        return $this->success('Title', $this->titleService->getById(
+        $getByIdResponse = $this->titleService->getById(
             $request->id
-        ));
+        );
+        if ($getByIdResponse->isSuccess()) {
+            return $this->success(
+                $getByIdResponse->getMessage(),
+                $getByIdResponse->getData(),
+                $getByIdResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getByIdResponse->getMessage(),
+                $getByIdResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetByDepartmentIdRequest $request
+     */
     public function getByDepartmentId(GetByDepartmentIdRequest $request)
     {
-        return $this->success('Titles', $this->titleService->getByDepartmentId(
+        $getByDepartmentIdResponse = $this->titleService->getByDepartmentId(
             $request->departmentId
-        ));
+        );
+        if ($getByDepartmentIdResponse->isSuccess()) {
+            return $this->success(
+                $getByDepartmentIdResponse->getMessage(),
+                $getByDepartmentIdResponse->getData(),
+                $getByDepartmentIdResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getByDepartmentIdResponse->getMessage(),
+                $getByDepartmentIdResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param CreateRequest $request
+     */
     public function create(CreateRequest $request)
     {
-        return $this->success('Title created', $this->titleService->create(
+        $createResponse = $this->titleService->create(
             $request->departmentId,
             $request->name
-        ));
+        );
+        if ($createResponse->isSuccess()) {
+            return $this->success(
+                $createResponse->getMessage(),
+                $createResponse->getData(),
+                $createResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $createResponse->getMessage(),
+                $createResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param UpdateRequest $request
+     */
     public function update(UpdateRequest $request)
     {
-        return $this->success('Title updated', $this->titleService->update(
+        $updateResponse = $this->titleService->update(
             $request->id,
             $request->name
-        ));
+        );
+        if ($updateResponse->isSuccess()) {
+            return $this->success(
+                $updateResponse->getMessage(),
+                $updateResponse->getData(),
+                $updateResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $updateResponse->getMessage(),
+                $updateResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param DeleteRequest $request
+     */
     public function delete(DeleteRequest $request)
     {
-        return $this->success('Title deleted', $this->titleService->delete(
+        $deleteResponse = $this->titleService->delete(
             $request->id
-        ));
+        );
+        if ($deleteResponse->isSuccess()) {
+            return $this->success(
+                $deleteResponse->getMessage(),
+                $deleteResponse->getData(),
+                $deleteResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $deleteResponse->getMessage(),
+                $deleteResponse->getStatusCode()
+            );
+        }
     }
 }
