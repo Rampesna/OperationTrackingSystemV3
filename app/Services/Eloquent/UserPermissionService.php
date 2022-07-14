e69ff9e440a96cdd183155edb1583dafbb39c937
Talha Can <rampesna@gmail.com>
2022-07-14 11:22:34 +0300

    v3.55.36.147

--- app/Services/Eloquent/UserPermissionService.php
+++ app/Services/Eloquent/UserPermissionService.php
@@ -4,32 +4,86 @@ namespace App\Services\Eloquent;
 
 use App\Interfaces\Eloquent\IUserPermissionService;
 use App\Models\Eloquent\UserPermission;
+use App\Services\ServiceResponse;
 
 class UserPermissionService implements IUserPermissionService
 {
-    public function getAll()
+    /**
+     * @return ServiceResponse
+     */
+    public function getAll(): ServiceResponse
     {
-        return UserPermission::all();
+        return new ServiceResponse(
+            true,
+            'All user permissions',
+            200,
+            UserPermission::all()
+        );
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function getById(
         int $id
-    )
+    ): ServiceResponse
     {
-        return UserPermission::find($id);
+        $userPermission = UserPermission::find($id);
+        if ($userPermission) {
+            return new ServiceResponse(
+                true,
+                'User permission',
+                200,
+                $userPermission
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'User permission not found',
+                404,
+                null
+            );
+        }
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function delete(
         int $id
-    )
+    ): ServiceResponse
     {
-        return $this->getById($id)->delete();
+        $userPermission = $this->getById($id);
+        if ($userPermission->isSuccess()) {
+            return new ServiceResponse(
+                true,
+                'User permission deleted',
+                200,
+                $userPermission->getData()->delete()
+            );
+        } else {
+            return $userPermission;
+        }
     }
 
+    /**
+     * @param int|null $topId
+     *
+     * @return ServiceResponse
+     */
     public function getByTopId(
         ?int $topId = null
-    )
+    ): ServiceResponse
     {
-        return UserPermission::where('top_id', $topId)->get();
+        return new ServiceResponse(
+            true,
+            'User permissions',
+            200,
+            UserPermission::where('top_id', $topId)->get()
+        );
     }
 }
