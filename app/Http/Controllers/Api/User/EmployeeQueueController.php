0f1232d1cbba4d2dbbcc5a054bf7f909ac2a8c0c
Talha Can <rampesna@gmail.com>
2022-07-14 17:05:49 +0300

    v3.55.36.233

--- app/Http/Controllers/Api/User/EmployeeQueueController.php
+++ app/Http/Controllers/Api/User/EmployeeQueueController.php
@@ -15,33 +15,107 @@ class EmployeeQueueController extends Controller
 {
     use Response;
 
+    /**
+     * @var $employeeService
+     */
     private $employeeService;
+
+    /**
+     * @var $queueService
+     */
     private $queueService;
 
-    public function __construct(IEmployeeService $employeeService, IQueueService $queueService)
+    /**
+     * @param IEmployeeService $employeeService
+     * @param IQueueService $queueService
+     */
+    public function __construct(
+        IEmployeeService $employeeService,
+        IQueueService    $queueService
+    )
     {
         $this->employeeService = $employeeService;
         $this->queueService = $queueService;
     }
 
+    /**
+     * @param GetEmployeeQueuesRequest $request
+     */
     public function getEmployeeQueues(GetEmployeeQueuesRequest $request)
     {
-        return $this->success('Employee queues', $this->employeeService->getEmployeeQueues($request->employeeId));
+        $getEmployeeQueuesResponse = $this->employeeService->getEmployeeQueues($request->employeeId);
+        if ($getEmployeeQueuesResponse->isSuccess()) {
+            return $this->success(
+                $getEmployeeQueuesResponse->getMessage(),
+                $getEmployeeQueuesResponse->getData(),
+                $getEmployeeQueuesResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getEmployeeQueuesResponse->getMessage(),
+                $getEmployeeQueuesResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param SetEmployeeQueuesRequest $request
+     */
     public function setEmployeeQueues(SetEmployeeQueuesRequest $request)
     {
-        return $this->success('Employee queues updated', $this->employeeService->setEmployeeQueues($request->employeeId, $request->queueIds));
+        $setEmployeeQueuesResponse = $this->employeeService->setEmployeeQueues($request->employeeId, $request->queueIds);
+        if ($setEmployeeQueuesResponse->isSuccess()) {
+            return $this->success(
+                $setEmployeeQueuesResponse->getMessage(),
+                $setEmployeeQueuesResponse->getData(),
+                $setEmployeeQueuesResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $setEmployeeQueuesResponse->getMessage(),
+                $setEmployeeQueuesResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetQueueEmployeesRequest $request
+     */
     public function getQueueEmployees(GetQueueEmployeesRequest $request)
     {
-        return $this->success('Queue employees', $this->queueService->getQueueEmployees($request->queueId));
+        $getQueueEmployeesResponse = $this->queueService->getQueueEmployees($request->queueId);
+        if ($getQueueEmployeesResponse->isSuccess()) {
+            return $this->success(
+                $getQueueEmployeesResponse->getMessage(),
+                $getQueueEmployeesResponse->getData(),
+                $getQueueEmployeesResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getQueueEmployeesResponse->getMessage(),
+                $getQueueEmployeesResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param SetQueueEmployeesRequest $request
+     */
     public function setQueueEmployees(SetQueueEmployeesRequest $request)
     {
-        return $this->success('Queue employees updated', $this->queueService->setQueueEmployees($request->queueId, $request->employeeIds));
+        $setQueueEmployeesResponse = $this->queueService->setQueueEmployees($request->queueId, $request->employeeIds);
+        if ($setQueueEmployeesResponse->isSuccess()) {
+            return $this->success(
+                $setQueueEmployeesResponse->getMessage(),
+                $setQueueEmployeesResponse->getData(),
+                $setQueueEmployeesResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $setQueueEmployeesResponse->getMessage(),
+                $setQueueEmployeesResponse->getStatusCode()
+            );
+        }
     }
 }
 
