349b49dfffd1574186eec0ba072337c3aaf8acd4
Talha Can <rampesna@gmail.com>
2022-07-14 15:46:49 +0300

    v3.55.36.202

--- app/Http/Controllers/Api/Employee/OvertimeTypeController.php
+++ app/Http/Controllers/Api/Employee/OvertimeTypeController.php
@@ -11,15 +11,36 @@ class OvertimeTypeController extends Controller
 {
     use Response;
 
+    /**
+     * @var $overtimeTypeService
+     */
     private $overtimeTypeService;
 
+    /**
+     * @param IOvertimeTypeService $overtimeTypeService
+     */
     public function __construct(IOvertimeTypeService $overtimeTypeService)
     {
         $this->overtimeTypeService = $overtimeTypeService;
     }
 
+    /**
+     * @param GetAllRequest $request
+     */
     public function getAll(GetAllRequest $request)
     {
-        return $this->success('Employee overtime types', $this->overtimeTypeService->getAll());
+        $getAllResponse = $this->overtimeTypeService->getAll();
+        if ($getAllResponse->isSuccess()) {
+            return $this->success(
+                $getAllResponse->getMessage(),
+                $getAllResponse->getData(),
+                $getAllResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getAllResponse->getMessage(),
+                $getAllResponse->getStatusCode()
+            );
+        }
     }
 }
