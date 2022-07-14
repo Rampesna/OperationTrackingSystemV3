513c4b740d219942519d3848d989664555f6b77b
Talha Can <rampesna@gmail.com>
2022-07-14 17:25:02 +0300

    v3.55.36.243

--- app/Http/Controllers/Api/User/TaskController.php
+++ app/Http/Controllers/Api/User/TaskController.php
@@ -18,60 +18,184 @@ class TaskController extends Controller
 {
     use Response;
 
+    /**
+     * @var $taskService
+     */
     private $taskService;
 
+    /**
+     * @param ITaskService $taskService
+     */
     public function __construct(ITaskService $taskService)
     {
         $this->taskService = $taskService;
     }
 
+    /**
+     * @param GetByIdRequest $request
+     */
     public function getById(GetByIdRequest $request)
     {
-        return $this->success('Task', $this->taskService->getById($request->id));
+        $getByIdResponse = $this->taskService->getById($request->id);
+        if ($getByIdResponse->isSuccess()) {
+            return $this->success(
+                $getByIdResponse->getMessage(),
+                $getByIdResponse->getData(),
+                $getByIdResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getByIdResponse->getMessage(),
+                $getByIdResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetFilesByIdRequest $request
+     */
     public function getFilesById(GetFilesByIdRequest $request)
     {
-        return $this->success('Task files', $this->taskService->getFilesById($request->id));
+        $getFilesByIdResponse = $this->taskService->getFilesById($request->id);
+        if ($getFilesByIdResponse->isSuccess()) {
+            return $this->success(
+                $getFilesByIdResponse->getMessage(),
+                $getFilesByIdResponse->getData(),
+                $getFilesByIdResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getFilesByIdResponse->getMessage(),
+                $getFilesByIdResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetSubTasksByIdRequest $request
+     */
     public function getSubTasksById(GetSubTasksByIdRequest $request)
     {
-        return $this->success('Task sub tasks', $this->taskService->getSubTasksById($request->id));
+        $getSubTasksByIdResponse = $this->taskService->getSubTasksById($request->id);
+        if ($getSubTasksByIdResponse->isSuccess()) {
+            return $this->success(
+                $getSubTasksByIdResponse->getMessage(),
+                $getSubTasksByIdResponse->getData(),
+                $getSubTasksByIdResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getSubTasksByIdResponse->getMessage(),
+                $getSubTasksByIdResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetCommentsByIdRequest $request
+     */
     public function getCommentsById(GetCommentsByIdRequest $request)
     {
-        return $this->success('Task comments', $this->taskService->getCommentsById($request->id));
+        $getCommentsByIdResponse = $this->taskService->getCommentsById($request->id);
+        if ($getCommentsByIdResponse->isSuccess()) {
+            return $this->success(
+                $getCommentsByIdResponse->getMessage(),
+                $getCommentsByIdResponse->getData(),
+                $getCommentsByIdResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getCommentsByIdResponse->getMessage(),
+                $getCommentsByIdResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param UpdateBoardRequest $request
+     */
     public function updateBoard(UpdateBoardRequest $request)
     {
-        return $this->success('Task', $this->taskService->updateBoard(
+        $updateBoardResponse = $this->taskService->updateBoard(
             $request->taskId,
             $request->boardId
-        ));
+        );
+        if ($updateBoardResponse->isSuccess()) {
+            return $this->success(
+                $updateBoardResponse->getMessage(),
+                $updateBoardResponse->getData(),
+                $updateBoardResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $updateBoardResponse->getMessage(),
+                $updateBoardResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param UpdateOrderRequest $request
+     */
     public function updateOrder(UpdateOrderRequest $request)
     {
-        return $this->success('Task', $this->taskService->updateOrder(
+        $updateOrderResponse = $this->taskService->updateOrder(
             $request->tasks
-        ));
+        );
+        if ($updateOrderResponse->isSuccess()) {
+            return $this->success(
+                $updateOrderResponse->getMessage(),
+                $updateOrderResponse->getData(),
+                $updateOrderResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $updateOrderResponse->getMessage(),
+                $updateOrderResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param UpdateByParametersRequest $request
+     */
     public function updateByParameters(UpdateByParametersRequest $request)
     {
-        return $this->success('Task', $this->taskService->updateByParameters(
+        $updateByParametersResponse = $this->taskService->updateByParameters(
             $request->id,
             $request->parameters
-        ));
+        );
+        if ($updateByParametersResponse->isSuccess()) {
+            return $this->success(
+                $updateByParametersResponse->getMessage(),
+                $updateByParametersResponse->getData(),
+                $updateByParametersResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $updateByParametersResponse->getMessage(),
+                $updateByParametersResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param DeleteRequest $request
+     */
     public function delete(DeleteRequest $request)
     {
-        return $this->success('Task deleted', $this->taskService->delete(
-            $request->id,
-        ));
+        $deleteResponse = $this->taskService->delete($request->id);
+        if ($deleteResponse->isSuccess()) {
+            return $this->success(
+                $deleteResponse->getMessage(),
+                $deleteResponse->getData(),
+                $deleteResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $deleteResponse->getMessage(),
+                $deleteResponse->getStatusCode()
+            );
+        }
     }
 }
