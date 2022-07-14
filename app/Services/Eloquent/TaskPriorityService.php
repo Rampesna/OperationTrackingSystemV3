9d1ddd65055634f7a966c1a8161dbce7ba264d61
Talha Can <rampesna@gmail.com>
2022-07-14 11:10:17 +0300

    v3.55.36.144

--- app/Services/Eloquent/TaskPriorityService.php
+++ app/Services/Eloquent/TaskPriorityService.php
@@ -4,26 +4,75 @@ namespace App\Services\Eloquent;
 
 use App\Interfaces\Eloquent\ITaskPriorityService;
 use App\Models\Eloquent\TaskPriority;
+use App\Services\ServiceResponse;
 
 class TaskPriorityService implements ITaskPriorityService
 {
-    public function getAll()
+    /**
+     * @return ServiceResponse
+     */
+    public function getAll(): ServiceResponse
     {
-        return TaskPriority::all();
+        return new ServiceResponse(
+            true,
+            'All task priorities',
+            200,
+            TaskPriority::all()
+        );
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function getById(
         int $id
-    )
+    ): ServiceResponse
     {
-        return TaskPriority::find($id);
+        $taskPriority = TaskPriority::find($id);
+        if ($taskPriority) {
+            return new ServiceResponse(
+                true,
+                'Task priority',
+                200,
+                $taskPriority
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Task priority not found',
+                404,
+                null
+            );
+        }
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function delete(
         int $id
-    )
+    ): ServiceResponse
     {
-        return $this->getById($id)->delete();
+        $taskPriority = $this->getById($id);
+        if ($taskPriority->isSuccess()) {
+            return new ServiceResponse(
+                true,
+                'Task priority deleted',
+                200,
+                $taskPriority->getData()->delete()
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Task priority not found',
+                404,
+                null
+            );
+        }
     }
 
 }
