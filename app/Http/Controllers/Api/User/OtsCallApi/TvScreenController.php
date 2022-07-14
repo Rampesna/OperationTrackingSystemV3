c322ddad7cc0e9cc6341d6120dddfd9fb1395711
Talha Can <rampesna@gmail.com>
2022-07-14 16:47:45 +0300

    v3.55.36.223

--- app/Http/Controllers/Api/User/OtsCallApi/TvScreenController.php
+++ app/Http/Controllers/Api/User/OtsCallApi/TvScreenController.php
@@ -4,21 +4,43 @@ namespace App\Http\Controllers\Api\User\OtsCallApi;
 
 use App\Http\Controllers\Controller;
 use App\Interfaces\OtsCallApi\ITvScreenService;
+use App\Http\Requests\Api\User\OtsCallApi\TvScreenController\GetSantralRequest;
 use App\Traits\Response;
 
 class TvScreenController extends Controller
 {
     use Response;
 
+    /**
+     * @var $tvScreenService
+     */
     private $tvScreenService;
 
+    /**
+     * @param ITvScreenService $tvScreenService
+     */
     public function __construct(ITvScreenService $tvScreenService)
     {
         $this->tvScreenService = $tvScreenService;
     }
 
-    public function getSantral()
+    /**
+     * @param GetSantralRequest $request
+     */
+    public function getSantral(GetSantralRequest $request)
     {
-        return $this->success('Santral', $this->tvScreenService->GetSantral());
+        $getSantralResponse = $this->tvScreenService->GetSantral();
+        if ($getSantralResponse->isSuccess()) {
+            return $this->success(
+                $getSantralResponse->getMessage(),
+                $getSantralResponse->getData(),
+                $getSantralResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getSantralResponse->getMessage(),
+                $getSantralResponse->getStatusCode()
+            );
+        }
     }
 }
