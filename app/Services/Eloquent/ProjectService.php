83f07c32fd44cbb3991135c7a90ca38312e2555c
Talha Can <rampesna@gmail.com>
2022-07-14 09:54:00 +0300

    v3.55.36.134

--- app/Services/Eloquent/ProjectService.php
+++ app/Services/Eloquent/ProjectService.php
@@ -4,92 +4,199 @@ namespace App\Services\Eloquent;
 
 use App\Interfaces\Eloquent\IProjectService;
 use App\Models\Eloquent\Project;
+use App\Services\ServiceResponse;
 
 class ProjectService implements IProjectService
 {
-    public function getAll()
+    /**
+     * @return ServiceResponse
+     */
+    public function getAll(): ServiceResponse
     {
-        return Project::with([
-
-        ])->all();
+        return new ServiceResponse(
+            true,
+            '',
+            200,
+            Project::all()
+        );
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function getById(
         int $id
-    )
+    ): ServiceResponse
     {
-        return Project::with([
+        $project = Project::with([
             'status'
-        ])->findOrFail($id);
+        ])->find($id);
+        if ($project) {
+            return new ServiceResponse(
+                true,
+                'Project',
+                200,
+                $project
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Project not found',
+                404,
+                null
+            );
+        }
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function delete(
         int $id
-    )
+    ): ServiceResponse
     {
-        return $this->getById($id)->delete();
+        $project = $this->getById($id);
+        if ($project->isSuccess()) {
+            return new ServiceResponse(
+                true,
+                'Project deleted',
+                200,
+                $project->getData()->delete()
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Project not found',
+                404,
+                null
+            );
+        }
     }
 
+    /**
+     * @param array $companyIds
+     *
+     * @return ServiceResponse
+     */
     public function getByCompanyIds(
         array $companyIds
-    )
+    ): ServiceResponse
     {
-        return Project::with([
-
-        ])->whereIn('company_id', $companyIds)->get();
+        return new ServiceResponse(
+            true,
+            'Projects',
+            200,
+            Project::whereIn('company_id', $companyIds)->get()
+        );
     }
 
+    /**
+     * @param array $projectIds
+     *
+     * @return ServiceResponse
+     */
     public function getByProjectIds(
         array $projectIds
-    )
+    ): ServiceResponse
     {
-        return Project::with([
-            'status'
-        ])->whereIn('id', $projectIds)->get();
+        return new ServiceResponse(
+            true,
+            'Projects',
+            200,
+            Project::whereIn('id', $projectIds)->get()
+        );
     }
 
     /**
      * @param int $projectId
+     *
+     * @return ServiceResponse
      */
     public function getSubtasksByProjectId(
         int $projectId
-    )
+    ): ServiceResponse
     {
-        return $this->getById($projectId)->subtasks()->get()->toArray();
+        $project = $this->getById($projectId);
+        if ($project->isSuccess()) {
+            return new ServiceResponse(
+                true,
+                'Subtasks',
+                200,
+                $project->getData()->subtasks()->get()->toArray()
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Project not found',
+                404,
+                null
+            );
+        }
     }
 
     /**
      * @param int $projectId
      * @param int $management
+     *
+     * @return ServiceResponse
      */
     public function getBoardsByProjectId(
         int $projectId,
         int $management
-    )
+    ): ServiceResponse
     {
-        return $this->getById($projectId)->boards()->with([
-            'tasks' => function ($tasks) {
-                $tasks->with([
-                    'subTasks',
-                    'priority'
-                ]);
-            }
-        ])->where('management', $management)->get()->toArray();
+        $project = $this->getById($projectId);
+        if ($project->isSuccess()) {
+            return new ServiceResponse(
+                true,
+                'Subtasks',
+                200,
+                $project->getData()->boards()->with([
+                    'tasks' => function ($tasks) {
+                        $tasks->with([
+                            'subTasks',
+                            'priority'
+                        ]);
+                    }
+                ])->where('management', $management)->get()->toArray()
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'Project not found',
+                404,
+                null
+            );
+        }
     }
 
     /**
      * @param array $projectIds
+     *
+     * @return ServiceResponse
      */
     public function getSubtasksByProjectIds(
         array $projectIds
-    )
+    ): ServiceResponse
     {
         $subtasks = [];
 
         foreach ($projectIds as $projectId) {
-            $subtasks = array_merge($subtasks, $this->getSubtasksByProjectId($projectId));
+            $projectSubtasks = $this->getSubtasksByProjectId($projectId);
+            if ($projectSubtasks->isSuccess()) {
+                $subtasks = array_merge($subtasks, $projectSubtasks->getData());
+            }
         }
 
-        return $subtasks;
+        return new ServiceResponse(
+            true,
+            'Subtasks',
+            200,
+            $subtasks
+        );
     }
 }
