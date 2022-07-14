23630f6f9429453d3ca602944307d76febec2424
Talha Can <rampesna@gmail.com>
2022-07-14 17:21:07 +0300

    v3.55.36.241

--- app/Http/Controllers/Api/User/SpecialReportController.php
+++ app/Http/Controllers/Api/User/SpecialReportController.php
@@ -11,15 +11,36 @@ class SpecialReportController extends Controller
 {
     use Response;
 
+    /**
+     * @var $specialReportService
+     */
     private $specialReportService;
 
+    /**
+     * @param ISpecialReportService $specialReportService
+     */
     public function __construct(ISpecialReportService $specialReportService)
     {
         $this->specialReportService = $specialReportService;
     }
 
+    /**
+     * @param GetByCompanyIdsRequest $request
+     */
     public function getByCompanyIds(GetByCompanyIdsRequest $request)
     {
-        return $this->success('Special reports', $this->specialReportService->getByCompanyIds($request->companyIds));
+        $getByCompanyIdsResponse = $this->specialReportService->getByCompanyIds($request->companyIds);
+        if ($getByCompanyIdsResponse->isSuccess()) {
+            return $this->success(
+                $getByCompanyIdsResponse->getMessage(),
+                $getByCompanyIdsResponse->getData(),
+                $getByCompanyIdsResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getByCompanyIdsResponse->getMessage(),
+                $getByCompanyIdsResponse->getStatusCode()
+            );
+        }
     }
 }
