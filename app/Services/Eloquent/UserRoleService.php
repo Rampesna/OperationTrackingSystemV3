748e423db80f4d970be6a010867df7a00611a507
Talha Can <rampesna@gmail.com>
2022-07-14 12:03:54 +0300

    v3.55.36.148

--- app/Services/Eloquent/UserRoleService.php
+++ app/Services/Eloquent/UserRoleService.php
@@ -4,24 +4,35 @@ namespace App\Services\Eloquent;
 
 use App\Interfaces\Eloquent\IUserRoleService;
 use App\Models\Eloquent\UserRole;
+use App\Services\ServiceResponse;
 
 class UserRoleService implements IUserRoleService
 {
-    public function getAll()
+    /**
+     * @return ServiceResponse
+     */
+    public function getAll(): ServiceResponse
     {
-        return UserRole::all();
+        return new ServiceResponse(
+            true,
+            'All user roles',
+            200,
+            UserRole::all()
+        );
     }
 
     /**
      * @param int $pageIndex
      * @param int $pageSize
      * @param string|null $keyword
+     *
+     * @return ServiceResponse
      */
     public function getAllUserRoles(
         int     $pageIndex = 0,
         int     $pageSize = 10,
         ?string $keyword = null
-    )
+    ): ServiceResponse
     {
         $userRoles = UserRole::with([]);
 
@@ -29,79 +40,161 @@ class UserRoleService implements IUserRoleService
             $userRoles->where('name', 'like', '%' . $keyword . '%');
         }
 
-        return [
-            'totalCount' => $userRoles->count(),
-            'pageIndex' => $pageIndex,
-            'pageSize' => $pageSize,
-            'userRoles' => $userRoles->skip($pageSize * $pageIndex)
-                ->take($pageSize)
-                ->get()
-        ];
+        return new ServiceResponse(
+            true,
+            'All user roles',
+            200,
+            [
+                'totalCount' => $userRoles->count(),
+                'pageIndex' => $pageIndex,
+                'pageSize' => $pageSize,
+                'userRoles' => $userRoles->skip($pageSize * $pageIndex)
+                    ->take($pageSize)
+                    ->get()
+            ]
+        );
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function getById(
         int $id
-    )
+    ): ServiceResponse
     {
-        return UserRole::find($id);
+        $userRole = UserRole::find($id);
+        if ($userRole) {
+            return new ServiceResponse(
+                true,
+                'User role',
+                200,
+                $userRole
+            );
+        } else {
+            return new ServiceResponse(
+                false,
+                'User role not found',
+                404,
+                null
+            );
+        }
     }
 
     /**
      * @param int $roleId
+     *
+     * @return ServiceResponse
      */
     public function getUserPermissions(
         int $roleId
-    )
+    ): ServiceResponse
     {
-        return UserRole::find($roleId)->userPermissions;
+        $userRole = $this->getById($roleId);
+        if ($userRole->isSuccess()) {
+            return new ServiceResponse(
+                true,
+                'User role permissions',
+                200,
+                $userRole->getData()->permissions
+            );
+        } else {
+            return $userRole;
+        }
     }
 
     /**
      * @param int $roleId
      * @param array $userPermissionIds
+     *
+     * @return ServiceResponse
      */
     public function setUserPermissions(
         int   $roleId,
         array $userPermissionIds
-    )
+    ): ServiceResponse
     {
-        return UserRole::find($roleId)->userPermissions()->sync($userPermissionIds);
+        $userRole = $this->getById($roleId);
+        if ($userRole->isSuccess()) {
+            $userRole->getData()->userPermissions()->sync($userPermissionIds);
+            return new ServiceResponse(
+                true,
+                'User role permissions updated',
+                200,
+                $userRole->getData()
+            );
+        } else {
+            return $userRole;
+        }
     }
 
     /**
      * @param string $name
+     *
+     * @return ServiceResponse
      */
     public function create(
         string $name
-    )
+    ): ServiceResponse
     {
         $userRole = new UserRole;
         $userRole->name = $name;
         $userRole->save();
 
-        return $userRole;
+        return new ServiceResponse(
+            true,
+            'User role created',
+            201,
+            $userRole
+        );
     }
 
     /**
      * @param int $id
      * @param string $name
+     *
+     * @return ServiceResponse
      */
     public function update(
         int    $id,
         string $name
-    )
+    ): ServiceResponse
     {
         $userRole = $this->getById($id);
-        $userRole->name = $name;
-        $userRole->save();
-
-        return $userRole;
+        if ($userRole->isSuccess()) {
+            $userRole->getData()->name = $name;
+            $userRole->getData()->save();
+            return new ServiceResponse(
+                true,
+                'User role updated',
+                200,
+                $userRole->getData()
+            );
+        } else {
+            return $userRole;
+        }
     }
 
+    /**
+     * @param int $id
+     *
+     * @return ServiceResponse
+     */
     public function delete(
         int $id
-    )
+    ): ServiceResponse
     {
-        return $this->getById($id)->delete();
+        $userRole = $this->getById($id);
+        if ($userRole->isSuccess()) {
+            return new ServiceResponse(
+                true,
+                'User role deleted',
+                200,
+                $userRole->getData()->delete()
+            );
+        } else {
+            return $userRole;
+        }
     }
 }
