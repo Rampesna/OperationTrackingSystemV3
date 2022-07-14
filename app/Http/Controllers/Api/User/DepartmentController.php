7d77a1b9279db7b01f58e9d73716f2554e4dd0ab
Talha Can <rampesna@gmail.com>
2022-07-14 17:01:08 +0300

    v3.55.36.231

--- app/Http/Controllers/Api/User/DepartmentController.php
+++ app/Http/Controllers/Api/User/DepartmentController.php
@@ -16,52 +16,148 @@ class DepartmentController extends Controller
 {
     use Response;
 
+    /**
+     * @var $departmentService
+     */
     private $departmentService;
 
+    /**
+     * @param IDepartmentService $departmentService
+     */
     public function __construct(IDepartmentService $departmentService)
     {
         $this->departmentService = $departmentService;
     }
 
+    /**
+     * @param GetAllRequest $request
+     */
     public function getAll(GetAllRequest $request)
     {
-        return $this->success('Departments', $this->departmentService->getAll());
+        $getAllResponse = $this->departmentService->getAll();
+        if ($getAllResponse->isSuccess()) {
+            return $this->success(
+                $getAllResponse->getMessage(),
+                $getAllResponse->getData(),
+                $getAllResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getAllResponse->getMessage(),
+                $getAllResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetByIdRequest $request
+     */
     public function getById(GetByIdRequest $request)
     {
-        return $this->success('Department', $this->departmentService->getById(
+        $getByIdResponse = $this->departmentService->getById(
             $request->id
-        ));
+        );
+        if ($getByIdResponse->isSuccess()) {
+            return $this->success(
+                $getByIdResponse->getMessage(),
+                $getByIdResponse->getData(),
+                $getByIdResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getByIdResponse->getMessage(),
+                $getByIdResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetByBranchIdRequest $request
+     */
     public function getByBranchId(GetByBranchIdRequest $request)
     {
-        return $this->success('Departments', $this->departmentService->getByBranchId(
+        $getByBranchIdResponse = $this->departmentService->getByBranchId(
             $request->branchId
-        ));
+        );
+        if ($getByBranchIdResponse->isSuccess()) {
+            return $this->success(
+                $getByBranchIdResponse->getMessage(),
+                $getByBranchIdResponse->getData(),
+                $getByBranchIdResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getByBranchIdResponse->getMessage(),
+                $getByBranchIdResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param CreateRequest $request
+     */
     public function create(CreateRequest $request)
     {
-        return $this->success('Department created', $this->departmentService->create(
+        $createResponse = $this->departmentService->create(
             $request->branchId,
             $request->name
-        ));
+        );
+        if ($createResponse->isSuccess()) {
+            return $this->success(
+                $createResponse->getMessage(),
+                $createResponse->getData(),
+                $createResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $createResponse->getMessage(),
+                $createResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param UpdateRequest $request
+     */
     public function update(UpdateRequest $request)
     {
-        return $this->success('Department updated', $this->departmentService->update(
+        $updateResponse = $this->departmentService->update(
             $request->id,
             $request->name
-        ));
+        );
+        if ($updateResponse->isSuccess()) {
+            return $this->success(
+                $updateResponse->getMessage(),
+                $updateResponse->getData(),
+                $updateResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $updateResponse->getMessage(),
+                $updateResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param DeleteRequest $request
+     */
     public function delete(DeleteRequest $request)
     {
-        return $this->success('Department deleted', $this->departmentService->delete(
+        $deleteResponse = $this->departmentService->delete(
             $request->id
-        ));
+        );
+        if ($deleteResponse->isSuccess()) {
+            return $this->success(
+                $deleteResponse->getMessage(),
+                $deleteResponse->getData(),
+                $deleteResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $deleteResponse->getMessage(),
+                $deleteResponse->getStatusCode()
+            );
+        }
     }
 }
