83268aa1548f4ba96ee2c0b7f87d847efa3b736b
Talha Can <rampesna@gmail.com>
2022-07-14 16:25:04 +0300

    v3.55.36.215

--- app/Http/Controllers/Api/User/OperationApi/OperationController.php
+++ app/Http/Controllers/Api/User/OperationApi/OperationController.php
@@ -23,66 +23,225 @@ class OperationController extends Controller
 {
     use Response;
 
+    /**
+     * @var $operationService
+     */
     private $operationService;
 
+    /**
+     * @param IOperationService $operationService
+     */
     public function __construct(IOperationService $operationService)
     {
         $this->operationService = $operationService;
     }
 
+    /**
+     * @param GetUserListRequest $request
+     */
     public function getUserList(GetUserListRequest $request)
     {
-        return $this->success('Operation users', $this->operationService->GetUserList($request->companyId));
+        $getUserListResponse = $this->operationService->GetUserList($request->companyId);
+        if ($getUserListResponse->isSuccess()) {
+            return $this->success(
+                $getUserListResponse->getMessage(),
+                $getUserListResponse->getData(),
+                $getUserListResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getUserListResponse->getMessage(),
+                $getUserListResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetEmployeeTasksRequest $request
+     */
     public function getEmployeeTasks(GetEmployeeTasksRequest $request)
     {
-        return $this->success('Employee tasks', $this->operationService->GetEmployeeTasks());
+        $getEmployeeTasksResponse = $this->operationService->GetEmployeeTasks();
+        if ($getEmployeeTasksResponse->isSuccess()) {
+            return $this->success(
+                $getEmployeeTasksResponse->getMessage(),
+                $getEmployeeTasksResponse->getData(),
+                $getEmployeeTasksResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getEmployeeTasksResponse->getMessage(),
+                $getEmployeeTasksResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetEmployeeTasksEditRequest $request
+     */
     public function getEmployeeTasksEdit(GetEmployeeTasksEditRequest $request)
     {
-        return $this->success('Employee tasks edit', $this->operationService->GetEmployeeTasksEdit($request->guid));
+        $getEmployeeTasksEditResponse = $this->operationService->GetEmployeeTasksEdit($request->guid);
+        if ($getEmployeeTasksEditResponse->isSuccess()) {
+            return $this->success(
+                $getEmployeeTasksEditResponse->getMessage(),
+                $getEmployeeTasksEditResponse->getData(),
+                $getEmployeeTasksEditResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getEmployeeTasksEditResponse->getMessage(),
+                $getEmployeeTasksEditResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param SetEmployeeTasksInsertRequest $request
+     */
     public function setEmployeeTasksInsert(SetEmployeeTasksInsertRequest $request)
     {
-        return $this->success('Employee tasks updated', $this->operationService->SetEmployeeTasksInsert($request->guid, $request->tasks));
+        $setEmployeeTasksInsertResponse = $this->operationService->SetEmployeeTasksInsert($request->guid, $request->tasks);
+        if ($setEmployeeTasksInsertResponse->isSuccess()) {
+            return $this->success(
+                $setEmployeeTasksInsertResponse->getMessage(),
+                $setEmployeeTasksInsertResponse->getData(),
+                $setEmployeeTasksInsertResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $setEmployeeTasksInsertResponse->getMessage(),
+                $setEmployeeTasksInsertResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetEmployeeWorkTasksRequest $request
+     */
     public function getEmployeeWorkTasks(GetEmployeeWorkTasksRequest $request)
     {
-        return $this->success('Employee work tasks', $this->operationService->GetEmployeeWorkTasks());
+        $getEmployeeWorkTasksResponse = $this->operationService->GetEmployeeWorkTasks();
+        if ($getEmployeeWorkTasksResponse->isSuccess()) {
+            return $this->success(
+                $getEmployeeWorkTasksResponse->getMessage(),
+                $getEmployeeWorkTasksResponse->getData(),
+                $getEmployeeWorkTasksResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getEmployeeWorkTasksResponse->getMessage(),
+                $getEmployeeWorkTasksResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetEmployeeWorkTasksEditRequest $request
+     */
     public function getEmployeeWorkTasksEdit(GetEmployeeWorkTasksEditRequest $request)
     {
-        return $this->success('Employee work tasks edit', $this->operationService->GetEmployeeWorkTasksEdit($request->guid));
+        $getEmployeeWorkTasksEditResponse = $this->operationService->GetEmployeeWorkTasksEdit($request->guid);
+        if ($getEmployeeWorkTasksEditResponse->isSuccess()) {
+            return $this->success(
+                $getEmployeeWorkTasksEditResponse->getMessage(),
+                $getEmployeeWorkTasksEditResponse->getData(),
+                $getEmployeeWorkTasksEditResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getEmployeeWorkTasksEditResponse->getMessage(),
+                $getEmployeeWorkTasksEditResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param SetEmployeeWorkTasksInsertRequest $request
+     */
     public function setEmployeeWorkTasksInsert(SetEmployeeWorkTasksInsertRequest $request)
     {
-        return $this->success('Employee work tasks updated', $this->operationService->SetEmployeeWorkTasksInsert($request->guid, $request->workTasks));
+        $setEmployeeWorkTasksInsertResponse = $this->operationService->SetEmployeeWorkTasksInsert($request->guid, $request->workTasks);
+        if ($setEmployeeWorkTasksInsertResponse->isSuccess()) {
+            return $this->success(
+                $setEmployeeWorkTasksInsertResponse->getMessage(),
+                $setEmployeeWorkTasksInsertResponse->getData(),
+                $setEmployeeWorkTasksInsertResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $setEmployeeWorkTasksInsertResponse->getMessage(),
+                $setEmployeeWorkTasksInsertResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetEmployeeGroupTasksRequest $request
+     */
     public function getEmployeeGroupTasks(GetEmployeeGroupTasksRequest $request)
     {
-        return $this->success('Employee group tasks', $this->operationService->GetEmployeeGroupTasks());
+        $getEmployeeGroupTasksResponse = $this->operationService->GetEmployeeGroupTasks();
+        if ($getEmployeeGroupTasksResponse->isSuccess()) {
+            return $this->success(
+                $getEmployeeGroupTasksResponse->getMessage(),
+                $getEmployeeGroupTasksResponse->getData(),
+                $getEmployeeGroupTasksResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getEmployeeGroupTasksResponse->getMessage(),
+                $getEmployeeGroupTasksResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetEmployeeGroupTasksEditRequest $request
+     */
     public function getEmployeeGroupTasksEdit(GetEmployeeGroupTasksEditRequest $request)
     {
-        return $this->success('Employee group tasks edit', $this->operationService->GetEmployeeGroupTasksEdit($request->guid));
+        $getEmployeeGroupTasksEditResponse = $this->operationService->GetEmployeeGroupTasksEdit($request->guid);
+        if ($getEmployeeGroupTasksEditResponse->isSuccess()) {
+            return $this->success(
+                $getEmployeeGroupTasksEditResponse->getMessage(),
+                $getEmployeeGroupTasksEditResponse->getData(),
+                $getEmployeeGroupTasksEditResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getEmployeeGroupTasksEditResponse->getMessage(),
+                $getEmployeeGroupTasksEditResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param SetEmployeeGroupTasksInsertRequest $request
+     */
     public function setEmployeeGroupTasksInsert(SetEmployeeGroupTasksInsertRequest $request)
     {
-        return $this->success('Employee group tasks updated', $this->operationService->SetEmployeeGroupTasksInsert($request->guid, $request->groupTasks));
+        $setEmployeeGroupTasksInsertResponse = $this->operationService->SetEmployeeGroupTasksInsert($request->guid, $request->groupTasks);
+        if ($setEmployeeGroupTasksInsertResponse->isSuccess()) {
+            return $this->success(
+                $setEmployeeGroupTasksInsertResponse->getMessage(),
+                $setEmployeeGroupTasksInsertResponse->getData(),
+                $setEmployeeGroupTasksInsertResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $setEmployeeGroupTasksInsertResponse->getMessage(),
+                $setEmployeeGroupTasksInsertResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param SetEmployeeRequest $request
+     */
     public function setEmployee(SetEmployeeRequest $request)
     {
-        return $this->success('Employee group tasks updated', $this->operationService->SetEmployee(
+        $setEmployeeResponse = $this->operationService->SetEmployee(
             $request->id,
             $request->companyId,
             $request->email,
@@ -106,21 +265,63 @@ class OperationController extends Controller
             $request->santralCode,
             $request->tasks,
             $request->workTasks
-        ));
+        );
+        if ($setEmployeeResponse->isSuccess()) {
+            return $this->success(
+                $setEmployeeResponse->getMessage(),
+                $setEmployeeResponse->getData(),
+                $setEmployeeResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $setEmployeeResponse->getMessage(),
+                $setEmployeeResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param GetDataScreeningRequest $request
+     */
     public function getDataScreening(GetDataScreeningRequest $request)
     {
-        return $this->success('Data screening', $this->operationService->GetDataScreening(
+        $getDataScreeningResponse = $this->operationService->GetDataScreening(
             $request->startDate,
             $request->endDate
-        ));
+        );
+        if ($getDataScreeningResponse->isSuccess()) {
+            return $this->success(
+                $getDataScreeningResponse->getMessage(),
+                $getDataScreeningResponse->getData(),
+                $getDataScreeningResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getDataScreeningResponse->getMessage(),
+                $getDataScreeningResponse->getStatusCode()
+            );
+        }
     }
 
+    /**
+     * @param SetStaffParameterRequest $request
+     */
     public function setStaffParameter(SetStaffParameterRequest $request)
     {
-        return $this->success('Set staff parameter', $this->operationService->SetStaffParameter(
+        $setStaffParameterResponse = $this->operationService->SetStaffParameter(
             $request->staffParameters
-        ));
+        );
+        if ($setStaffParameterResponse->isSuccess()) {
+            return $this->success(
+                $setStaffParameterResponse->getMessage(),
+                $setStaffParameterResponse->getData(),
+                $setStaffParameterResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $setStaffParameterResponse->getMessage(),
+                $setStaffParameterResponse->getStatusCode()
+            );
+        }
     }
 }
