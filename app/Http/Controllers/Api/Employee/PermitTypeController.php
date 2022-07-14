d65723ff93d06538976de3c65f1637c868fd0234
Talha Can <rampesna@gmail.com>
2022-07-14 15:56:20 +0300

    v3.55.36.206

--- app/Http/Controllers/Api/Employee/PermitTypeController.php
+++ app/Http/Controllers/Api/Employee/PermitTypeController.php
@@ -11,15 +11,36 @@ class PermitTypeController extends Controller
 {
     use Response;
 
+    /**
+     * @var $permitTypeService
+     */
     private $permitTypeService;
 
+    /**
+     * @param IPermitTypeService $permitTypeService
+     */
     public function __construct(IPermitTypeService $permitTypeService)
     {
         $this->permitTypeService = $permitTypeService;
     }
 
+    /**
+     * @param GetAllRequest $request
+     */
     public function getAll(GetAllRequest $request)
     {
-        return $this->success('Employee permitTypes', $this->permitTypeService->getAll());
+        $getAllResponse = $this->permitTypeService->getAll();
+        if ($getAllResponse->isSuccess()) {
+            return $this->success(
+                $getAllResponse->getMessage(),
+                $getAllResponse->getData(),
+                $getAllResponse->getStatusCode()
+            );
+        } else {
+            return $this->error(
+                $getAllResponse->getMessage(),
+                $getAllResponse->getStatusCode()
+            );
+        }
     }
 }
