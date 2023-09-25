a7330876e29b499abcbc11a67e816a02ff36c145
Talha Can <rampesna@gmail.com>
2023-09-25 16:02:52 +0300

    v3.155.151.832
    Suspended employee create market payment blocked.

--- app/Http/Controllers/Api/Employee/MarketPaymentController.php
+++ app/Http/Controllers/Api/Employee/MarketPaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Employee;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Employee\MarketPaymentController\CreateRequest;
+use App\Interfaces\Eloquent\IEmployeeService;
 use App\Interfaces\Eloquent\IMarketPaymentService;
 use App\Traits\Response;
 
@@ -17,11 +18,21 @@ class MarketPaymentController extends Controller
     private $marketPaymentService;
 
     /**
-     * @param IMarketPaymentService $marketPaymentService
+     * @var $employeeService
      */
-    public function __construct(IMarketPaymentService $marketPaymentService)
+    private $employeeService;
+
+    /**
+     * @param IMarketPaymentService $marketPaymentService
+     * @param IEmployeeService $employeeService
+     */
+    public function __construct(
+        IMarketPaymentService $marketPaymentService,
+        IEmployeeService      $employeeService
+    )
     {
         $this->marketPaymentService = $marketPaymentService;
+        $this->employeeService = $employeeService;
     }
 
     /**
@@ -29,26 +40,41 @@ class MarketPaymentController extends Controller
      */
     public function create(CreateRequest $request)
     {
-        $createResponse = $this->marketPaymentService->create(
-            null,
-            null,
-            $request->user()->id,
-            'App\Models\Eloquent\Employee',
-            $request->amount,
-            str_pad(mt_rand(0, 99999999), 8, '0', STR_PAD_LEFT),
-            1,
-            0
-        );
-        if ($createResponse->isSuccess()) {
-            return $this->success(
-                $createResponse->getMessage(),
-                $createResponse->getData(),
-                $createResponse->getStatusCode()
-            );
+        $employee = $this->employeeService->getById($request->user()->id);
+        if ($employee->isSuccess()) {
+            if ($employee->getData()->suspend == 1) {
+                return $this->error(
+                    'Your account is suspended',
+                    403
+                );
+            } else {
+                $createResponse = $this->marketPaymentService->create(
+                    null,
+                    null,
+                    $request->user()->id,
+                    'App\Models\Eloquent\Employee',
+                    $request->amount,
+                    str_pad(mt_rand(0, 99999999), 8, '0', STR_PAD_LEFT),
+                    1,
+                    0
+                );
+                if ($createResponse->isSuccess()) {
+                    return $this->success(
+                        $createResponse->getMessage(),
+                        $createResponse->getData(),
+                        $createResponse->getStatusCode()
+                    );
+                } else {
+                    return $this->error(
+                        $createResponse->getMessage(),
+                        $createResponse->getStatusCode()
+                    );
+                }
+            }
         } else {
             return $this->error(
-                $createResponse->getMessage(),
-                $createResponse->getStatusCode()
+                $employee->getMessage(),
+                $employee->getStatusCode()
             );
         }
     }
